aceffe7569179892de5e2767e12448d6e9d4472f
Rafael Costa <rafaelcosta8020@gmail.com>
2024-05-21 22:02:11 -0300

    teste

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            By{' '}
+            By Teste
             <Image
               src="/vercel.svg"
               alt="Vercel Logo"
