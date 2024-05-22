242bd456b38ee7e85795dcc516485c3949e70cd1
Rafael Costa <rafaelcosta8020@gmail.com>
2024-05-22 20:31:20 -0300

    feat: add initial pages

---
+++ src/app/mapa/page.tsx
@@ -0,0 +1,7 @@
+export default function Mapa() {
+  return (
+    <>
+      <div>Página mapa</div>
+    </>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main>
-      <div></div>
+      <div>Página principal</div>
     </main>
   );
 }
