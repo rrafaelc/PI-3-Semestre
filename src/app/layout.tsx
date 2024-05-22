8673f729462a68ac96787736a00980ee89b59986
Rafael Costa <rafaelcosta8020@gmail.com>
2024-05-22 20:24:36 -0300

    feat: add favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,6 +18,7 @@ export default function RootLayout({
   return (
     <html lang="pt-br">
       <body className={roboto.className}>
+        <link rel="icon" href="/images/logo.png" />
         <Header />
         {children}
       </body>
